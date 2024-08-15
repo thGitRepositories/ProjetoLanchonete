8cec910ecf08fb8d2433679fd7640b0654b2e00d
thGitRepositories <thiagoemp16@outlook.com>
2024-08-15 11:19:16 -0300

    initial commit

---
+++ src/main/java/ProjetoLanchonete/com/App.java
@@ -0,0 +1,13 @@
+package ProjetoLanchonete.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
